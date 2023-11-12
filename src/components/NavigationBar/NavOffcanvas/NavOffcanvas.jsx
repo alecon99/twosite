3cfdbb169d454ecb3113f8e2e008cf8cdefbca74
alecon99 <alessioconte45@gmail.com>
2023-11-12 15:29:48 +0100

    add social navigationBar

--- src/components/NavigationBar/NavOffcanvas/NavOffcanvas.jsx
+++ src/components/NavigationBar/NavOffcanvas/NavOffcanvas.jsx
@@ -4,10 +4,11 @@ import { nanoid } from 'nanoid'
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import { faArrowCircleLeft, faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-router-dom';
 
-const NavOffcanvas = ({links}) => {
+const NavOffcanvas = ({ links }) => {
 
     const [show, setShow] = useState(false);
 
@@ -16,26 +17,36 @@ const NavOffcanvas = ({links}) => {
 
     return (
         <>
-        <div onClick={handleShow} className='text-white'>
-            <FontAwesomeIcon icon={faBars} />
-        </div>
-        
-        <Offcanvas show={show} onHide={handleClose} className='bg-black text-white'>
-            <Offcanvas.Header>
-                <div onClick={handleClose}>
-                    <FontAwesomeIcon icon={faX} />
-                </div>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-            {links.map((link) => {
-            return (
-                <div key={nanoid()} className='border-bottom p-2'> 
-                    <Link to={link.href} className='link_reset fs-4'>{link.name}</Link>
-                </div>
-            )
-          })}
-            </Offcanvas.Body>
-        </Offcanvas>
+            <div onClick={handleShow} className='text-white'>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+
+            <Offcanvas show={show} onHide={handleClose} className='bg-black text-white'>
+                <Offcanvas.Header className='fs-3'>
+                    <div onClick={handleClose}>
+                        <FontAwesomeIcon icon={faArrowCircleLeft} />
+                    </div>
+                    <Link to={"/"} className='link_reset'>
+                        HOME
+                    </Link>
+                </Offcanvas.Header>
+                <Offcanvas.Body className='d-flex flex-column justify-content-between'>
+                    <div>
+                        {links.map((link) => {
+                            return (
+                                <div key={nanoid()} className='border-bottom p-2'>
+                                    <Link to={link.href} className='link_reset fs-1'>{link.name}</Link>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className='border-top pt-3 d-flex justify-content-evenly fs-1'>
+                        <FontAwesomeIcon icon={faInstagram} />
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
         </>
     )
 }
